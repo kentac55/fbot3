0ad9d5edfa79c0b08ac94bfd646e532a04f19453
kentac55 <dev.kentac55@gmail.com>
2019-06-03 00:39:38 +0900

    マッチポンプ機能追加

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { RTMClient, WebClient } from '@slack/client'
+import { CronJob } from 'cron'
 
 import { getTokenFromEnv, getClients, getChannel } from './utils'
 import pino from 'pino'
@@ -6,7 +7,23 @@ import { registerEventHandlers } from './slack/index'
 
 const logger = pino()
 const [rtm, web]: [RTMClient, WebClient] = getClients(getTokenFromEnv())
+const channel = getChannel()
 
 rtm.start()
 
-registerEventHandlers(rtm, web, logger.child({ module: 'slack' }), getChannel())
+registerEventHandlers(rtm, web, logger.child({ module: 'slack' }), channel)
+
+const job = new CronJob(
+  // At 09:00 on every day-of-week from Monday through Friday.
+  '00 00 9 * * 1-5',
+  (): void => {
+    web.chat.postMessage({
+      channel,
+      text: '$ ojichat',
+      as_user: true,
+      link_names: true,
+    })
+  }
+)
+
+job.start()
