8024353b23fb1651bc1951ef433ad97afd0782eb
kentac55 <dev.kentac55@gmail.com>
2019-06-06 00:49:34 +0900

    reactivate cron

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { RTMClient, WebClient } from '@slack/client'
-// import { CronJob } from 'cron'
+import { CronJob } from 'cron'
 
 import { getTokenFromEnv, getClients, getChannel } from './utils'
 import pino from 'pino'
@@ -13,17 +13,17 @@ rtm.start()
 
 registerEventHandlers(rtm, web, logger.child({ module: 'slack' }), channel)
 
-// const job = new CronJob(
-//   // At 09:00 on every day-of-week from Monday through Friday.
-//   '00 00 00 * * 1-5',
-//   (): void => {
-//     web.chat.postMessage({
-//       channel,
-//       text: '$ help',
-//       as_user: true,
-//       link_names: true,
-//     })
-//   }
-// )
-//
-// job.start()
+const job = new CronJob(
+  // At 09:00 on every day-of-week from Monday through Friday.
+  '00 00 00 * * 1-5',
+  (): void => {
+    web.chat.postMessage({
+      channel,
+      text: '$ help',
+      as_user: true,
+      link_names: true,
+    })
+  }
+)
+
+job.start()
